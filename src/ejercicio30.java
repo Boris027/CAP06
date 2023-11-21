795c7eeb066d9d8d1ac591fbf1b8da006d0ae487
Boris027 <titankiller027@gmail.com>
2023-11-21 11:47:59 +0100

    subida casi 30

--- src/ejercicio30.java
+++ src/ejercicio30.java
@@ -1,47 +1,22 @@
 public class ejercicio30 {
 
+    public static void bordes(int ancho){
+        for(int k=1;k<=ancho;k++){
+                    System.out.print("*");
+                }
+                System.out.println("");
+    }
+
    public static int pedirnum(){
     int numero=Integer.parseInt(System.console().readLine());
+     
+    
     return numero;
    }
 
 
 
-   public static void anchura(int ancho){
-
-    for(int i=1;i<=ancho;i++){
-        System.out.print("* ");
-    
-    }
-    System.out.println("");
-   }
-
-
-
-
-   public static void espacios(int ancho, int pez){
-
-
-    for(int i=1;i<=ancho*2-3;i++){
-
-        if(i==pez){
-            System.out.print("&");
-        }else System.out.print(" ");
-
-    }
-   }
-
-   public static void espacios2(int ancho, int pez){
-
-
-    for(int i=1;i<=ancho*2-3;i++){
-
-        if(i==pez){
-            System.out.print("@");
-        }else System.out.print("");
-
-    }
-   }
+  
 
    
 
@@ -54,45 +29,46 @@ public class ejercicio30 {
             System.out.print("Por favor, introduzca la altura de la pecera (como mínimo 4): ");
             int altura=pedirnum();
             System.out.print("Ahora introduzca la anchura (como mínimo 4): ");
-            int ancho=pedirnum();
+            int anchura=pedirnum();
+
+
+
+            /* */
+            
+            int randomalto=(int) (Math.random()*(altura-2) +1);
+            int randomancho=(int) (Math.random()*(anchura-2) +1);
+            
+            int randomalto2=(int) (Math.random()*(altura-2) +1);
+            int randomancho2=0;
+            do {
+                 randomancho2=(int) (Math.random()*(anchura-2) +1);
+            } while (randomalto2==randomalto && randomancho==randomancho2);
+            
 
             
-            int alturarandom=(int) (Math.random()*(altura-2)+1);
-            int espaciorandom=(int) (Math.random()*(ancho*2-3) +1);
 
 
+            bordes(anchura);
+            for(int i=1;i<=altura-2;i++){
 
-            int espaciorandom2=espaciorandom;
-            int alturarandom2=(int) (Math.random()*(altura-2)+1);
-            if(espaciorandom==espaciorandom2 && alturarandom==alturarandom2){
+                
 
-                while(espaciorandom2==espaciorandom){
-                    espaciorandom2=(int) (Math.random()*(ancho*2-3) +1);
+                System.out.print("*");
+                for(int j=1;j<=anchura-2;j++){
+
+                    if(randomalto==i && randomancho==j){
+                        System.out.print("&");
+                    }else if(randomalto2==i && randomancho2==j){
+                        System.out.print("@");
+                    }else System.out.print(" ");
+
+
                 }
+                System.out.println("*");
+
             }
-             
 
-            int alturarandom3=(int) (Math.random()*(altura-2)+1);
-            int espaciorandom3=(int) (Math.random()*(ancho*2-3) +1);
-            
-            
-            
-            
-            
-            anchura(ancho);
-
-          for(int i=1;i<=altura-2;i++){
-
-            System.out.print("*");
-            if(i==alturarandom){
-                espacios(ancho, espaciorandom);
-            }else espacios(ancho,-5);
-
-            System.out.print("*");
-            System.out.println("");
-            
-          }
-          anchura(ancho);
+            bordes(anchura);
 
 
             
